36153c12915a19851218444e162ceb265f9cc70a
parthmakadiya12 <parthmakadiya12@gmail.com>
2021-04-10 00:11:06 +0200

    Add phone layout and basic styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Header } from "./components/Header";
 import "./App.css";
 
 function App() {
-  return <div className="App">Currency converter</div>;
+  return (
+    <div className="App">
+      <div class="main">
+        <Header />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,3 @@
+export const Header = () => {
+  return <h1 class="header">Convertio</h1>;
+};
